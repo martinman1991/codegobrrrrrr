600e04a285dc54a2bc480735bd2243079885b0c4
martinman1991 <98027494+martinman1991@users.noreply.github.com>
2022-03-23 10:09:26 +0100

    Add files via upload

---
+++ dawd.cs
@@ -0,0 +1,9 @@
+namespace AhyesHelloHiWergWewWhatWo
+{
+    class LuaBatPyCAsmCpp {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("https://www.youtube.com/watch?v=p68mzXLmCac");
+        }
+    }
+}
